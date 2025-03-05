7ae4254b845a5d886e5d7b6cdc8d056c70a68d70
Tomas-SilvaC <tommy2006silva@gmail.com>
2025-03-05 18:20:26 -0500

    Update main.cpp

--- Laboratorio_ArbolesBinarios/main.cpp
+++ Laboratorio_ArbolesBinarios/main.cpp
@@ -3,25 +3,30 @@ Tomas Silva
 Laboratorio de Arbol binario
 5 de Marzo de 2025
 *******************************************************************************/
-
-#include <iostream>
 #include "ModArbol/ModArbol.h"
+#include <iostream>
+
 using namespace std;
 
 int main()
 {
     //Construye el arbol de Prueba
     struct nodo *arbol0=new nodo(7);
+
     arbol0->left =new nodo(3);
     arbol0->right =new nodo(20);
+
     arbol0->left->left =new nodo(0);
     arbol0->left->right =new nodo(5);
+
     arbol0->left->left->left =new nodo(-3);
     arbol0->left->left->right =new nodo(1);
     arbol0->left->right->left =new nodo(4);
     arbol0->left->right->right =new nodo(6);
+
     arbol0->right->left =new nodo(15);
     arbol0->right->right =new nodo(25);
+
     arbol0->right->right->right =new nodo(30);
 
     //Realiza los procesos
@@ -33,7 +38,7 @@ int main()
     
     cout<< endl <<"PostOrden: ";
     PostOrder (arbol0);
-    
+    cout<< endl;
     
     return 0;
 }
