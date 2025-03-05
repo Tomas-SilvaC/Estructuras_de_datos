150e6ac350da982f41a084f3c50d916949ba73e2
Tomas-SilvaC <tommy2006silva@gmail.com>
2025-03-05 13:23:06 -0500

    Update main.cpp

--- Laboratorio_ArbolesBinarios/main.cpp
+++ Laboratorio_ArbolesBinarios/main.cpp
@@ -10,18 +10,30 @@ using namespace std;
 
 int main()
 {
-    struct nodo *arbol0=new nodo(23);
-    arbol0->left =new nodo(2);
-    arbol0->right =new nodo(42);
-    arbol0->left->left =new nodo(-1);
-    arbol0->left->right =new nodo(8);
+    //Construye el arbol de Prueba
+    struct nodo *arbol0=new nodo(7);
+    arbol0->left =new nodo(3);
+    arbol0->right =new nodo(20);
+    arbol0->left->left =new nodo(0);
+    arbol0->left->right =new nodo(5);
+    arbol0->left->left->left =new nodo(-3);
+    arbol0->left->left->right =new nodo(1);
+    arbol0->left->right->left =new nodo(4);
+    arbol0->left->right->right =new nodo(6);
+    arbol0->right->left =new nodo(15);
+    arbol0->right->right =new nodo(25);
+    arbol0->right->right->right =new nodo(30);
+
+    //Realiza los procesos
+    cout<< endl <<"InOrden: ";
+    InOrder (arbol0);
     
     cout<< endl <<"PreOrden: ";
     PreOrder (arbol0);
+    
     cout<< endl <<"PostOrden: ";
     PostOrder (arbol0);
-    cout<< endl <<"InOrden: ";
-    InOrder (arbol0);
+    
     
     return 0;
 }
