c976a75a5ac804bf3a799cbf08433de501b42e21
Tomas-SilvaC <tommy2006silva@gmail.com>
2025-03-10 18:50:13 -0500

    Update rectangle.h

--- Taller_1/Código/rectangle.h
+++ Taller_1/Código/rectangle.h
@@ -1,13 +1,13 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
-
+//Crea la estructura rectangulo
 struct Rectangle {
   float posX;
   float posY;
   int width;
   int height;	
 };
-
+//Inicializa los metodos
 float perimeterRect ( Rectangle rect );
 float areaRect ( Rectangle rect );
 float distOriginRect ( Rectangle rect );
