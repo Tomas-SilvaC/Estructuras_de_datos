ad833ded291cee70fb209767cb52a9ce303fc9dc
Tomas-SilvaC <tommy2006silva@gmail.com>
2025-03-10 18:45:15 -0500

    Update exercise2.cxx

--- Taller_1/Código/exercise2.cxx
+++ Taller_1/Código/exercise2.cxx
@@ -1,4 +1,4 @@
-// exercise1.cxx (Andrea Rueda)
+// exercise1.cxx (Andrea Rueda) - creadora del codigo original
 
 // Main program that uses the functionality of rectangle.h
 
@@ -11,7 +11,7 @@ int main () {
 
 	Rectangle rect1;
 	int a, b;
-
+	//Pide datos relevantes para el calculo con metodos de rectangulo
 	cout << "Ingrese coordenada X de la posicion del rectangulo: ";
 	cin >> rect1.posX;
 	cout << "Ingrese coordenada Y de la posicion del rectangulo: ";
@@ -20,7 +20,7 @@ int main () {
 	cin >> rect1.width;
 	cout << "Ingrese alto del rectangulo: ";
 	cin >> rect1.height;
-
+	//Saca los resultados impresos en orden de perimetro, area y distancia
 	cout << "\nPerimetro del rectangulo: " << perimeterRect( rect1 ) << endl;
 	cout << "Area del rectangulo: " << areaRect( rect1 ) << endl;
 	cout << "Distancia del rectangulo al origen de coordenadas: " << distOriginRect( rect1 ) << endl;
